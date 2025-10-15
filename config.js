dd72e1a2261a1bd69e7cc3f6678a8870cabe7ac5
Ludovic Fournier <fouludovic@gmail.com>
2025-10-15 00:21:58 -0400

    dix

--- config.js
+++ config.js
@@ -4,15 +4,12 @@ export const ALLOWEDORIGINSPROD = ["https://map.mtltrainlive.com"];
 // Allow connection in dev mod
 export const ALLOWEDORIGINSDEV = [
   "http://127.0.0.1:5500",
+  "https://127.0.0.1:5500",
   "http://map.mtltrainlive.com",
   "https://map.mtltrainlive.com",
 ];
 
-export const ALLOWEDORIGINHEADERS = [
-  "Content-Type",
-  "Authorization",
-  "X-Train-Info",
-];
+export const ALLOWEDORIGINHEADERS = ["Content-Type", "X-Train-Info"];
 
 // EXO API CALL interval
 export const EXOAPICALLINTERVAL = 30; //Value in second
