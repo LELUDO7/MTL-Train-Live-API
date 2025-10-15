8b3d05dfef914fffdd7cc173dd51847c86b4bf1b
Ludovic Fournier <fouludovic@gmail.com>
2025-10-14 23:55:03 -0400

    fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // File name    : app.js
 // Created by   : Ludovic Fournier
 // Created date : 09-09-2025
-// Description  : app use
+// Description  : app use 
 
 import express from "express";
 import cors from "cors";
@@ -12,18 +12,32 @@ import { ALLOWEDORIGINSDEV, ALLOWEDORIGINSPROD } from "../config.js";
 
 const app = express();
 
-// gobale middlewares
+// gobale middlewares 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//This is for the local testing with the Web live map project
-
-app.use(
-  cors({
-    origin: "*",
-    allowedHeaders: ["Content-Type", "Authorization", "train_info"],
-  })
-);
+//This is for the local testing with the Web live map project 
+if (env.dev) {
+    app.use(cors({
+    origin: function (origin, callback) {
+    if (!origin || ALLOWEDORIGINSDEV.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+    }
+    }));
+} else {
+    app.use(cors({
+    origin: function (origin, callback) {
+    if (!origin || ALLOWEDORIGINSPROD.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+    }
+    }));
+}
 
 // routes
 app.use("/api/", routes);
