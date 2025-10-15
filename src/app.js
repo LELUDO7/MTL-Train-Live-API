eeadc410871e84a319bd13f8572d30b307ecec1f
Ludovic Fournier <fouludovic@gmail.com>
2025-10-15 00:24:40 -0400

    fixxx

--- src/app.js
+++ src/app.js
@@ -1,43 +1,54 @@
 // File name    : app.js
 // Created by   : Ludovic Fournier
 // Created date : 09-09-2025
-// Description  : app use 
+// Description  : app use
 
 import express from "express";
 import cors from "cors";
 import { notFound, errorHandler } from "./middlewares/error.js";
 import routes from "./routes/route.js";
 import { env } from "./config/env.js";
-import { ALLOWEDORIGINHEADERS, ALLOWEDORIGINSDEV, ALLOWEDORIGINSPROD } from "../config.js";
+import {
+  ALLOWEDORIGINHEADERS,
+  ALLOWEDORIGINSDEV,
+  ALLOWEDORIGINSPROD,
+} from "../config.js";
 
 const app = express();
 
-// gobale middlewares 
+// gobale middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//This is for the local testing with the Web live map project 
+//This is for the local testing with the Web live map project
 if (env.dev) {
-    app.use(cors({
-    allowedHeaders: ALLOWEDORIGINHEADERS,
-    origin: function (origin, callback) {
-    if (!origin || ALLOWEDORIGINSDEV.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-    }
-    }));
+  app.use(
+    cors({
+      cache: "no-store",
+      allowedHeaders: ALLOWEDORIGINHEADERS,
+      origin: function (origin, callback) {
+        if (!origin || ALLOWEDORIGINSDEV.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+    })
+  );
 } else {
-    app.use(cors({
-    origin: function (origin, callback) {
-    if (!origin || ALLOWEDORIGINSPROD.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-    }
-    }));
+  app.use(
+    cors({
+      cache: "no-store",
+      allowedHeaders: ALLOWEDORIGINHEADERS,
+      origin: function (origin, callback) {
+        if (!origin || ALLOWEDORIGINSPROD.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+    })
+  );
 }
 
 // routes
