140c6a9c1e3d5c2c0c524080d9a7a5a070fed2ef
Ludovic Fournier <fouludovic@gmail.com>
2025-12-09 15:43:55 -0500

    little change now consits are in corsant order

--- src/services/consists.service.js
+++ src/services/consists.service.js
@@ -10,6 +10,9 @@ export async function listConsists(line) {
   if (line == 1 || ( 3 <=line && line <= 6)) {
     try {
       const data = await consist.find({ line: line });
+      data.sort(
+        (a, b) => Number(a.trip_short_name) - Number(b.trip_short_name)
+      );
       return data;
     } catch (err) {
       log.error("Faild to find consists :", err);
