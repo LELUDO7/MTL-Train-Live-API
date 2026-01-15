053898ba47387085bddcd6c3227ea434a4c8ad22
Ludovic Fournier <fouludovic@gmail.com>
2026-01-14 22:22:09 -0500

    test

--- src/services/stations.service.js
+++ src/services/stations.service.js
@@ -11,6 +11,7 @@ import { TRAIN_ENGINE } from "../data/train.engine.js"
 
 export function listStations(trainInfo) {
   let data = getLiveData();
+  console.log(data);
   if (data.entity == null) {
     return [];
   }
